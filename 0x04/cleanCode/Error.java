342940ce1eb142c95393cfb24f440533cc294eb1
Ever Santoro - LYNCAS <ever.santoro.ext@fibrasil.com.br>
2024-12-16 09:16:28 -0300

    v3

--- 0x04/cleanCode/Error.java
+++ 0x04/cleanCode/Error.java
@@ -1,34 +1,72 @@
 public class Error {
 
+
     // Use Exceptions ao invés de retornar o código
-    public User createUser(User user) {
+    //A
+    public User createUser(User user){
         try {
             loadUser(user.getId());
-            throw new UserExistsException(user.getId());
-        } catch (UserNotFoundException exception) {
+            throw new UserExistsException(user.getId())
+        }
+        catch (UserNotFoundException exception){
             // detail here
         }
         resolveGroup(user);
         return userRepository.save(user);
     }
+    //B
+    public Object createUser(User user){
 
-    // Exceções com contexto
-    public void validate(String id) {
-        if (ownerRepo.count(id) == 0) {
-            throw new ValidationException("id: " + id + " não foi informado, deveria ser informado...");
+        if(loadUser(user.getId()) instanceof User){
+            // user already exists
+            return USER_ALREADY_EXISTS; // error flag
+            return 101; // or error code
+        };
+
+        resolveGroup(user);
+        return userRepository.save(user);
+    }
+
+    // Exceções com contexto    
+    //A
+    public void validate(String id){
+        if(ownerRepo.count(id) == 0){
+            throw new ValidationException("ocorreu uma exceção ...")
+        }
+    }
+    // B
+    public void validate(String id){
+        if(ownerRepo.count(id) == 0){
+            throw new ValidationException("id: " + id + " não foi informado, deveria ser informado...")
         }
     }
 
-    // Não retorne null e não passe null
-    public User createUserHandlingNull(User user) {
+
+    // Não retorne nulle não passe null
+    //A
+    public User createUser(User user){
+
+        if(loadUser(user.getId()) instanceof User){
+            // user already exists
+            return null;
+        };
+
+        resolveGroup(user);
+        return userRepository.save(user);
+    }
+    //B
+    public User createUser(User user){
+
         try {
             loadUser(user.getId());
-            throw new UserExistsException(user.getId());
-        } catch (UserNotFoundException exception) {
+            throw new UserExistsException(user.getId())
+        }
+        catch (UserNotFoundException exception){
             // detail here
         }
 
         resolveGroup(user);
         return userRepository.save(user);
     }
+
 }
