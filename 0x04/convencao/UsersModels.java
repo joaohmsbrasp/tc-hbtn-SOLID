eb8978c11004e3f5c775fbe8afd6d3557cb63604
Ever Santoro - LYNCAS <ever.santoro.ext@fibrasil.com.br>
2024-12-16 09:52:39 -0300

    v1

---
+++ 0x04/convencao/UsersModels.java
@@ -0,0 +1,25 @@
+// Classe UserModel, representando nossa entidade
+
+public class UserModel {
+
+    private int idUser;
+
+    private String descricao;
+
+    private static final int IDADE_MINIMA = 18;
+
+    // Método que coleta todos os usuários
+    public List<UserModel> fetchUsers() {
+        return new ArrayList<>();
+    }
+
+    // Método que coleta todos os usuários
+    public List<UserModel> allUsers() {
+        return new ArrayList<>();
+    }
+
+    // Método que coleta todos os usuários
+    public List<UserModel> getUsers() {
+        return new ArrayList<>();
+    }
+}
